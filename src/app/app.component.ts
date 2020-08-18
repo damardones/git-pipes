5a1fa064ac035cd6633b5e5175b18ec10e04f932
Daniel Mardones <dm.dev@outlook.com>
2020-08-18 00:07:33 -0400

    Proyecto pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre: string = 'Capitán América';
+  nombre2: string = 'dANIel maRDONEs aGuAYo';
+  arreglo:string[] = ['Aquaman', 'Batman', 'Spiderman', 'Wolverine', 'Flash', 'Hulk', 'Daredevil'];
+  PI:number = Math.PI;
+  porcentaje:number = 0.234;
+  salario:number = 1234.5;
+  fecha: Date = new Date();
+  idioma:string = 'es';
+
+  valorPromesa = new Promise<string>((resolve) => {
+    
+    setTimeout(() => {
+      resolve('llego la data');
+    },4500);
+    
+  });
+
+  heroe = {
+    nombre: 'logan',
+    clave: 'Wolvering',
+    edad: 500,
+    direccion:{
+      calle: 'Primera',
+      casa: 20 
+    }
+  }
+
+  setIdioma(idioma:string){
+    this.idioma = idioma;
+  }
 }
